c5f6cde99335d7f5df567015e91ded75face4f08
Oliverinthenight <145535155+Oliverinthenight@users.noreply.github.com>
2024-09-12 20:24:02 +0200

    Rep Step 1 Initialization
    Followed the frist 9 steps on the first assignment part. Got implemented the fundemental class libraries and interfaces for the upcoming steps

---
+++ Entities/Comment.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id { get; set; }
+    public string Body { get; set; }
+    public int PostId { get; set; }  // Foreign key - Post
+    public int UserId { get; set; }  
+}
---
+++ RepositoryContracts/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace RepositoryContracts;
+
+public interface IRepository<T>
+{
+    void Create(T entity);
+    T GetById(int id);
+    IEnumerable<T> GetAll();
+    void Update(T entity);
+    void Delete(int id);
+}
+ 
